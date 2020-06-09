d43c561844853c0755c447183286c1fa611f4c70
Barbara Bontempo <barbarabontempo94@gmail.com>
2020-06-09 15:32:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -108,8 +108,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-    programmer_hash[:known_for] = "GUI"
-    p programmer_hash
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+    programmer_hash
 end
 
 
